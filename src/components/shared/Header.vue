cbb48f462035049dc960ca358a4ccbcbdda58d73
João Paulo Mazzo <mazzojp@gmail.com>
2023-01-01 19:10:22 -0300

    reupdate entire repo

---
+++ src/components/shared/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <RouterLink to="/">
+    <img src="../../assets/shared/logo.svg" alt="payapi logo" />
+  </RouterLink>
+
+  <nav>
+    <RouterLink to="/">Pricing</RouterLink>
+    <RouterLink to="/">About</RouterLink>
+    <RouterLink to="/">Contact</RouterLink>
+  </nav>
+</template>
