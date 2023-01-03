d01637e82918e8559fe7cae9a4468451fc900bae
João Paulo Mazzo <mazzojp@gmail.com>
2023-01-02 21:39:23 -0300

    Add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { RouterView } from "vue-router";
+
 import Header from "./components/shared/HeaderComponent.vue";
+import Footer from "./components/shared/FooterComponent.vue";
 </script>
 
 <template>
   <Header />
   <RouterView />
+  <Footer />
 </template>
