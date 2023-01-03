43ba099c021067fbed67cc79b036d7908c6e0bca
João Paulo Mazzo <mazzojp@gmail.com>
2023-01-02 21:38:03 -0300

    Add Navigation component

--- src/components/shared/HeaderComponent.vue
+++ src/components/shared/HeaderComponent.vue
@@ -1,20 +1,8 @@
 <template>
   <header
-    class="w-4/5 mx-auto my-10 flex flex-row items-center justify-between font-public-sans font-bold bg-transparent"
+    class="w-4/5 mx-auto py-10 flex flex-row items-center justify-between font-public-sans font-bold bg-transparent"
   >
-    <div class="flex gap-16 items-center">
-      <router-link to="/">
-        <img src="../../assets/shared/logo.svg" alt="payapi logo" />
-      </router-link>
-
-      <nav
-        class="flex gap-10 t text-[#36536B] text-opacity-70 hover:text-opacity-100"
-        v-for="(item, index) of navItems"
-        :key="index"
-      >
-        <router-link :to="item.url">{{ item.label }}</router-link>
-      </nav>
-    </div>
+    <CreateNavigation :header="true" />
 
     <button
       type="button"
@@ -26,24 +14,11 @@
 </template>
 
 <script>
+import CreateNavigation from "./CreateNavigation.vue";
+
 export default {
-  data() {
-    return {
-      navItems: [
-        {
-          url: "/pricing",
-          label: "Pricing",
-        },
-        {
-          url: "/about",
-          label: "About",
-        },
-        {
-          url: "/contact",
-          label: "Contact",
-        },
-      ],
-    };
+  components: {
+    CreateNavigation,
   },
 };
 </script>
